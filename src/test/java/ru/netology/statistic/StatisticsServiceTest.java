1791c982feb098f19cfb7a34da7f352e908251d8
zosha1 <82464653+zosha1@users.noreply.github.com>
2021-05-31 15:21:18 +0300

    commit5

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
